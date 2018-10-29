d09dcddcf789b7a6a27643df64b9ffd44fb09144
Sami B <sboleyn@email.arizona.edu>
2018-10-29 13:59:22 -0700

    changes to design and functionality

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,7 +1,20 @@
 var express = require("express");
 
-var burger = require("../models/burger.js");
-
 var router = express.Router();
 
+var burger = require("../models/burger.js");
+
+router.get("/index", function(req, res) {
+    burger.selectAll(function(data) {
+        var burgersObj = {
+            burger: data
+        };
+        console.log(burgersObj);
+        res.render("index", burgersObj);
+    });
+
+})
+
 module.exports = router;
+
+// route to index
