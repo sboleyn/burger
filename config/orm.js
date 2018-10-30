ff03acb85a48e6052fc683c9595d7a60095b89ea
Sami B <sboleyn@email.arizona.edu>
2018-10-29 17:57:36 -0700

    finished

--- config/orm.js
+++ config/orm.js
@@ -2,35 +2,62 @@ var connection = require("./connection.js");
 
 // ?? is used for table and column names, it escapes them with backticks. ? is for ordinary values.
 
+function objToSql(ob) {
+    var arr = [];
+
+    // loop through the keys and push the key/value as a string int arr
+    for (var key in ob) {
+        var value = ob[key];
+        // check to skip hidden properties
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // if string with spaces, add quotations (Lana Del Grey => 'Lana Del Grey')
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+            // e.g. {name: 'Lana Del Grey'} => ["name='Lana Del Grey'"]
+            // e.g. {sleepy: true} => ["sleepy=true"]
+            arr.push(key + "=" + value);
+        }
+    }
+
+    // translate array of strings to a single comma-separated string
+    return arr.toString();
+}
+
 var orm = {
-    selectAll: function (tableInput, cb) {
+    selectAll: function(tableInput, cb) {
         var queryString = "SELECT * FROM ??";
-        connection.query(queryString, [tableInput], function (err, result) {
+        connection.query(queryString, [tableInput], function(err, result) {
             if (err) {
                 throw err;
 
-            }
-            else {
+            } else {
                 cb(result)
             }
         });
     },
-    insertOne: function (tableInput, vals, cb) {
-        var queryString = "INSERT INTO ?? VALUES ?";
-        connection.query(queryString, [tableInput, vals], function (err, result) {
-            if (err) { throw err }
-            else {
+    insertOne: function(tableInput, vals, cb) {
+
+        var queryString = "INSERT INTO ??(burger_name) VALUES (?)";
+        connection.query(queryString, [tableInput, vals], function(err, result) {
+            if (err) { throw err } else {
                 cb(result)
             }
         });
     },
-    updateOne: function (tableInput, objColVals, condition, cb) {
-        var queryString = "UPDATE ?? SET ?? WHERE ?";
-        connection.query(queryString, [tableInput, objColVals, condition], function (err, result) {
+    updateOne: function(tableInput, objColVals, condition, cb) {
+        var queryString = "UPDATE " + tableInput;
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+        // console.log(objToSql(objColVals));
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
             if (err) {
                 throw err;
-            }
-            else {
+            } else {
+                console.log(queryString);
                 cb(result)
             }
         });
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -15,6 +15,29 @@ router.get("/index", function(req, res) {
 
 })
 
+router.post("/burgers", function(req, res) {
+    console.log(req.body.burger_name)
+    burger.insertOne([req.body.burger_name],
+        function(result) {
+            res.json({ id: result.insertId });
+        }
+    )
+})
+
+router.put("/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+
+    burger.updateOne({ devoured: req.body.devoured }, condition,
+        function(result) {
+            if (result.changedRows == 0) {
+                return res.status(404).end();
+            } else {
+                res.status(200).end();
+            }
+        }
+    )
+})
+
 module.exports = router;
 
 // route to index
